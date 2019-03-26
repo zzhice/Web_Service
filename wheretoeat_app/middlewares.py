4f89a8c50219275500f996f7214ee573fd82f7df
zhaoziha@umich.edu <zhaoziha@umich.edu>
2019-03-26 19:54:17 -0400

    remove all old files

--- wheretoeat_app/middlewares.py
+++
@@ -1,9 +0,0 @@
-from django.http import QueryDict
-
-def put_middleware(get_responese):
-    def middleware(request):
-        if request.method == 'PUT':
-            setattr(request, 'PUT', QueryDict(request.body))
-        response = get_responese(request)
-        return response
-    return middleware
--- wheretoeat_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class basicModel(models.Model):
-    name = models.CharField(max_length=50)
-    code = models.TextField()
-
-    def __str__(self):
-        return '{},{}'.format(self.name, self.code)
